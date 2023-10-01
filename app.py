078e6a1e76a3da05544747c5ea019b614197d531
Uzezi Henry Ovraiti <uovraiti@yahoo.com>
2023-10-01 02:24:10 +0100

    added get video

--- app.py
+++ app.py
@@ -60,14 +60,14 @@ def update_video(video_id):
 # Endpoint to get video data
 
 
-@app.route('/get_video/<int:video_id>', methods=['GET'])
+@@app.route('/get_video/<int:video_id>', methods=['GET'])
 def get_video(video_id):
-    # Return the video file path
-    video = videos.get(video_id)
+    video = Video.query.get(video_id)
     if video:
-        return jsonify({'file_path': video['file_path']})
+        return jsonify({'file_path': video.file_path})
     else:
         return jsonify({'error': 'Video not found.'}), 404
+
 
 
 if __name__ == '__main__':
